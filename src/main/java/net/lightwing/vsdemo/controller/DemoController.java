f0b7e92b39d48006a9c910708906452ddfb9fc35
Clarence <clarence@lightwing.net>
2019-04-18 11:01:34 +0800

    First Commit

---
+++ src/main/java/net/lightwing/vsdemo/controller/DemoController.java
@@ -0,0 +1,20 @@
+package net.lightwing.vsdemo.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * DemoController
+ */
+@RestController
+public class DemoController{
+    @RequestMapping("/hello")
+    public Map<String,Object> Hello(){
+        Map<String,Object> map = new HashMap<>();
+        map.put("name", "Visual Studio Code");
+        return map;
+    }
+}
